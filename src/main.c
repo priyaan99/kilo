db4135a8a5a603382cb3a95420e207ccba2d6974
priyaan99 <priyaanyadav@gmail.com>
2022-12-31 09:22:05 +0530

    clear screen & keyprosses

--- src/main.c
+++ src/main.c
@@ -1,14 +1,27 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+
 struct termios original_termios;
 
+/*** terminal ***/
+
 void die(const char *s) {
+    // clear screen
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 } 
@@ -38,18 +51,50 @@ void enable_raw_mode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editor_read_key() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) {
+            die("read");
+        }
+    }
+    return c;
+}
+
+/*** output ***/
+
+void editor_refresh_screen() {
+    // clear screen
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    // reposition the cursor
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** input ***/
+
+void editor_process_keypress() {
+    char c = editor_read_key();
+
+    switch (c) {
+        case CTRL_KEY('q'): {
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+        }
+        break;
+    }
+}
+
+/*** init ***/
+
 int main() {
     enable_raw_mode();
 
-    while (true) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-
-        if (c == 'q') break;
+    while (1) {
+        editor_refresh_screen();
+        editor_process_keypress();
     }
+
+    return 0;
 }
